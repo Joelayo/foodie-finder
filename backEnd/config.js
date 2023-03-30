57553c884baf074a95c47e403e5905e69e01628c
Joel Oduyemi <64360534+Joelayo@users.noreply.github.com>
2023-03-30 01:29:21 +0100

    Delete config.js

--- backEnd/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config(); // Import environment variables from .env file
-
-module.exports = {
-  PORT: process.env.PORT || 4000, // Set port for Express server
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://test:testuser@foodiefinder.sy2joof.mongodb.net/?retryWrites=true&w=majority', // Set MongoDB connection URI
-  JWT_SECRET: process.env.JWT_SECRET || 'secretkey', // Set JWT secret key
-};
